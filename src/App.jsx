f299f2f9e9912283eeb64858e28062754120d269
Virendra Ghule <113795394+Virendra108@users.noreply.github.com>
2024-03-09 10:21:51 +0000

    changed name and info

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Virendra Ghule",
+  title: "CSE undergrad",
+  email: "virendra0252@gmail.com",
+  gitHub: "Virendra108",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "virendra-ghule",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
